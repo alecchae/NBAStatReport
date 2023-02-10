b2fe46dcbeddf91fda4a98e7c0d50fa7513efb27
Alec Chae <59631218+alecchae@users.noreply.github.com>
2023-02-10 16:39:00 -0700

    player id being printed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import sys
 from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.static import players
 import pandas as pd
 
 def print_hi(name):
@@ -8,7 +10,13 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    input = sys.argv[1] + " " + sys.argv[2]
+    print(input)
     career = playercareerstats.PlayerCareerStats(player_id='203999')
-    career.get_data_frames()[0]
-    r = career.get_json()
-    print(r)
+    r = career.get_data_frames()[0]
+    nba_players = players.get_players()
+
+    big_fundamental = [player for player in nba_players
+                       if player['full_name'] == input]
+    print("player id = " + str(big_fundamental[0]['id']))
+
